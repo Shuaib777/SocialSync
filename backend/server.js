21ad2469369af6702dfac6c8fd516ea051c3a45d
Shuaib777 <shuaibahmedshaikh7@gmail.com>
2025-08-28 02:09:59 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,7 @@ const allowedOrigins = ["http://localhost:5173", process.env.CLIENT_URL];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
--- frontend/src/context/SocketsContext.jsx
+++ frontend/src/context/SocketsContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if (!user?._id) return;
 
-    const newSocket = io(API_URL);
+    const newSocket = io(API_URL || "http://localhost:5000");
     setSocket(newSocket);
 
     newSocket.emit("register", user._id);
